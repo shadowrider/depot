17079ee7400e1b3bbb9f3cd7ea2e1c8316a3df1c
Myroslav Pomazan <myroslav.pom@gmail.com>
2011-07-09 15:26:25 -0700

    Validation!

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,9 @@
 class Product < ActiveRecord::Base
+  validates :title, :description, :image_url, :presence => true
+  validates :price, :numericality => {:greater_or_equal_to => 0.01}
+  validates :title, :uniqueness => true
+  validates :image_url, :format => {
+    :with => %r{\.(gif|png|jpg)$}i,
+    :message => 'Must be a URL to GIF, PNG or JPG picture'
+  }
 end
--- test/unit/product_test.rb
+++ test/unit/product_test.rb
@@ -1,8 +1,65 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
+  fixtures :products
   # Replace this with your real tests.
   test "the truth" do
     assert true
+  end
+  
+  test "product attributes must not be empty" do
+    product = Product.new
+    assert product.invalid?
+    assert product.errors[:title].any?
+    assert product.errors[:description].any?
+    assert product.errors[:image_url].any?
+    assert product.errors[:price].any?
+  end
+  
+  test "product price must be positive" do
+    product = Product.new(:title => "title",
+                          :description => "description here",
+                          :image_url => "asda.jpg")
+    product.price = -1
+    assert product.invalid?
+    assert_equal "Must be greater or equal to 0.01",
+      product.errors[:price].join('; ')
+      
+    product.price = 0
+    assert product.invalid?
+    assert_equal "Must be greater or equal to 0.01",
+      product.errors[:price].join('; ')
+    
+    product.price = 1
+    assert product.valid?
+  end
+  
+  def new_product(image_url)
+    Product.new(:title => "some title",
+                :description => "some desctription",
+                :image_url => image_url,
+                :price => 10)
+  end
+  
+  test "Image URL must be correct" do
+    ok = %w{fred.gif fred.png trula.jpg MYRKA.gif http://myrka.com/image/buuu.jpg}
+    bad = %w{asdm.dsf lolka.jpeg some.doc}
+    
+    ok.each do |url|
+      assert new_product(url).valid?, "#{url} should not be invalid"
+    end
+    
+    bad.each do |url|
+      assert new_product(url).invalid?, "#{url} should not be valid"
+    end
+  end
+  
+  test "product is not valid withour a unique title" do
+    product = Product.new(:title => products(:ruby).title,
+                          :description => "some description",
+                          :image_url => products(:ruby).image_url,
+                          :price => 2)
+    assert !product.save
+    assert_equal "has already been taken", product.errors[:title].join('; ')
   end
 end
